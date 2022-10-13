672f2359cdceee6f044393fe56279b85bfdf6ca9
Evgeny Petrov <petroves88@mail.ru>
2022-10-13 11:46:49 +0300

    add Example 005 Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("What is your name? : ");
+string username = Console.ReadLine();
+if (username.ToLower() == "evgeny"){
+    Console.WriteLine("Good Job Evgeny!");
+} else {
+    Console.Write("Bad Job ");
+    Console.WriteLine(username);
+}
